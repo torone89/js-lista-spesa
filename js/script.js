255caf4a6b25da72121da06c8a874b84b3740b57
torone89 <torone89>
2022-06-28 14:21:34 +0200

    Esercizio Completo for

--- js/script.js
+++ js/script.js
@@ -3,4 +3,25 @@
 console.log("JS")
 
 
+// Reupero ul dal dom "listaspesa"
 
+const listaspesa = document.getElementById('listastampa')
+console.log(listaspesa)
+
+// Creo Array 
+const shop = ["Uova", "Pane", "Latte", "Biscotti", "Pasta", "Affettati", "Formaggio", "Verdura", "Frutta",];
+console.log(shop)
+
+let lista = ''
+
+// mi creo il mio ciclo for per controllare tutta la mia listaitem
+for (let i = 0; i < shop.length; i++) {
+    lista += `<li>${shop[i]}</li>`;
+    console.log(shop[i])
+
+}
+
+console.log(lista)
+// stampo nel Dom la lista della spesa
+
+listaspesa.innerHTML = lista;
